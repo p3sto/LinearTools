09b794ec4a9d82a4e85b005aca6bc390684cc5b6
_Pesto <pestonotpasta@gmail.com>
2025-03-13 11:46:34 -0600

    Begin cli planning

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,44 @@
+use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
-use structopt::StructOpt;
-
 mod converter;
+mod worker;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "linear-tools")]
+    /**
+     * linear-tools
+     *      Commands:
+     *          linear - Converts from anvil to linear format
+     *          anvil  - Converts from linear to anvil format
+     *          verify - Verifies a directory of linear files
+     *      
+     *      Required:
+     *          <src> - Source file or directory
+     *      
+     *      Optional:
+     *          --compression-level - The zstd compression level to use for linear conversions      
+     *          --workers - Number of worker processes to use (Default to core count * 2)
+     *          --metrics - Provides metrics and progress report
+     *          --output  - Output processed regions to another directory
+     */
+
+#[derive(Parser)]
+#[command(name = "linear-tools")]
 struct Cli {
-    #[structopt(
-        short,
-        long,
-        help = "Converts regions to the Anvil (MCA) format",
-        hide_default_value = true
-    )]
+    #[arg(long, help="Converts from anvil to linear format")]
     mca: bool,
 
-    #[structopt(
-        short,
-        long,
-        help = "Converts regions to the Linear format",
-        hide_default_value = true
-    )]
+    #[arg(long, )]
     linear: bool,
 
-    #[structopt(
-        short,
-        long,
-        default_value = "0",
-        help = "Sets the verbosity level of the output",
-        hide_default_value = true
-    )]
-    verbose: u16,
-
-    #[structopt(
-        short,
-        long,
-        default_value = "1",
-        help = "Sets the number of threads to use for conversion",
-        hide_default_value = true
-    )]
-    threads: u16,
-
-    #[structopt(
-        short,
-        long,
-        default_value = "6",
-        help = "Sets compression level for linear conversion (1-22)",
-        hide_default_value = true
-    )]
-    compression: u8,
-
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
-
-    #[structopt(short = "i", long = "input", help = "")]
-    output: Option<PathBuf>,
 }
 
-fn validate_args(args: &mut Cli) {
-    // Validate conversion method
-    if args.linear && args.mca {
-        eprintln!("Error: --mca and --linear cannot both be enabled",);
-        std::process::exit(1);
-    }
-
-    // Set default thread count to the number of cpus
-    if args.threads == 0 {
-        args.threads = num_cpus::get() as u16;
-    }
+enum Command {
+    
 }
 
 fn main() {
-    let mut cli = Cli::from_args();
-    validate_args(&mut cli);
+    let cli = Cli::parse();
 
-    print!("{:?}", cli)
+    println!("Linear? {:?}", cli.linear);
+    println!("Mca? {:?}", cli.mca);
 }
