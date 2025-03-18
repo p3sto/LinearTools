537dcfa25d5c4c5672d77353deffdce573bb577b
_Pesto <pestonotpasta@gmail.com>
2025-03-18 12:01:56 -0600

    Began parallelization of conversion taks

--- src/worker.rs
+++ src/worker.rs
@@ -1,4 +1,37 @@
+use rayon::prelude::*;
+use std::time::Instant;
 
-struct Worker {
-    threads: usize
+use crate::converter::*;
+
+pub struct Worker {
+    queue: Vec<String>,
+    threads: usize,
+    from: ConverterType,
+    to: ConverterType,
+}
+
+impl Worker {
+    pub fn new(queue: Vec<String>, threads: usize, from: ConverterType, to: ConverterType) -> Self {
+        Self {
+            queue,
+            threads,
+            from,
+            to,
+        }
+    }
+
+    pub fn run(&self) {
+        let start = Instant::now();
+
+        self.queue.par_iter().for_each(|file| {
+            let region = match self.from {
+                ConverterType::Anvil => todo!(),
+                ConverterType::LinearV1(_) => todo!(),
+                _ => (),
+            };
+        });
+
+        let end = Instant::now();
+        let time = end - start;
+    }
 }
