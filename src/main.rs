6a4f5d55f9515d23c6eb8efab5a2dd4865712612
_Pesto <pestonotpasta@gmail.com>
2025-03-18 12:03:27 -0600

    Set up command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,59 @@
-use clap::{Parser, Subcommand};
 use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use converter::ConverterType;
 
 mod converter;
 mod worker;
 
-    /**
-     * linear-tools
-     *      Commands:
-     *          linear - Converts from anvil to linear format
-     *          anvil  - Converts from linear to anvil format
-     *          verify - Verifies a directory of linear files
-     *      
-     *      Required:
-     *          <src> - Source file or directory
-     *      
-     *      Optional:
-     *          --compression-level - The zstd compression level to use for linear conversions      
-     *          --workers - Number of worker processes to use (Default to core count * 2)
-     *          --metrics - Provides metrics and progress report
-     *          --output  - Output processed regions to another directory
-     */
+/**
+ * linear-tools
+ *      Commands:
+ *          linear - Converts from anvil to linear format
+ *          anvil  - Converts from linear to anvil format
+ *          verify - Verifies a directory of linear files
+ *
+ *      Required:
+ *          <src> - Source file or directory
+ *
+ *      Optional:
+ *          --compression-level - The zstd compression level to use for linear conversions
+ *          --workers - Number of worker processes to use (Default to core count * 2)
+ *          --metrics - Provides metrics and progress report
+ *          --output  - Output processed regions to another directory
+ */
 
 #[derive(Parser)]
 #[command(name = "linear-tools")]
 struct Cli {
-    #[arg(long, help="Converts from anvil to linear format")]
-    mca: bool,
+    #[arg(short, long, help = "The source file or directory to process files")]
+    input: PathBuf,
 
-    #[arg(long, )]
-    linear: bool,
+    #[arg(
+        short,
+        long,
+        help = "Specifies the number of worker threads for conversion"
+    )]
+    workers: Option<u8>,
 
+    #[arg(
+        short = 'm',
+        long = "track-metrics",
+        help = "Provides metrics and status updates"
+    )]
+    track_metrics: bool,
 }
 
+#[derive(Subcommand)]
 enum Command {
-    
+    Convert {
+        from: ConverterType,
+        to: ConverterType,
+        output: Option<PathBuf>,
+    },
+    Verify,
 }
 
 fn main() {
     let cli = Cli::parse();
-
-    println!("Linear? {:?}", cli.linear);
-    println!("Mca? {:?}", cli.mca);
 }
